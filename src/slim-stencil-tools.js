ea07c6ad120632b50b222a237f667b7d019b48d8
Donny West <donnywest@gmail.com>
2016-12-15 08:53:11 -0500

    try different export strategy

--- src/slim-stencil-tools.js
+++ src/slim-stencil-tools.js
@@ -1,23 +1,29 @@
-import * as Base from './api/Base';
-import * as Page from './api/Page';
-import * as Cart from './api/Cart';
-import * as Product from './api/Product';
-import imageUrl from './lib/image-url';
+// import * as Base from './api/Base';
+// import * as Page from './api/Page';
+// import * as Cart from './api/Cart';
+// import * as Product from './api/Product';
+// import imageUrl from './lib/image-url';
 
-const slimStencilTools = {
-  Base,
-  Page,
-  Cart,
-  Product,
-  imageUrl,
-};
+// const slimStencilTools = {
+//   Base,
+//   Page,
+//   Cart,
+//   Product,
+//   imageUrl,
+// };
 
-export {
-  Base,
-  Page,
-  Cart,
-  Product,
-  imageUrl,
-};
+// export {
+//   Base,
+//   Page,
+//   Cart,
+//   Product,
+//   imageUrl,
+// };
 
-export default slimStencilTools;
+// export default slimStencilTools;
+
+export * as Base from './api/Base';
+export * as Page from './api/Page';
+export * as Cart from './api/Cart';
+export * as Product from './api/Product';
+export imageUrl from './lib/image-url';
